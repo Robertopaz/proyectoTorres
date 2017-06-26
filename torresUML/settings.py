a99fd25abe8f5cc5fdf200daa6a63ee97c421c05
RPazPerez <robertopaz@msn.com>
2017-06-26 12:55:28 -0500

    elimina static de config file

--- torresUML/settings.py
+++ torresUML/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'gitData',
 ]
 
@@ -119,4 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = ''

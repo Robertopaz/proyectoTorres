f2528e5f38382cb724ee1b9e80a07b37adfdc6cd
Alan Malagón <miris_rock2009@hotmail.com>
2017-06-27 17:02:06 -0500

    Correción de obtención de código de métodos
    Arreglé errores que impedía obtener bien los códigos de clases intermedias y de la clase final que obtenía todo el código sobrante al no encontrar más "def"

--- gitData/lecturaPython.py
+++ gitData/lecturaPython.py
@@ -9,44 +9,45 @@ def setCodigo(pCodigo):
     global codigo
     codigo = pCodigo
 
-"""setCodigo(class Olakease:
-    def _init_(self,edad, peso, altura):
-        self.edad = edad
-        self.peso = peso
-        self.altura = altura
+setCodigo("""class Telefono:
+    "Clase teléfono"
+    def __init__(self):
+        pass
+    def telefonear(self):
+        print('llamando')
+    def colgar(self):
+        print('colgando')        
 
-    def kamara(self,mensaje):
-        print mensaje
+class Camara:
+    "Clase camara fotográfica"
+    def __init__(self):
+        pass
+    def fotografiar(self):
+        print('fotografiando')        
 
-    def kamara(self,mensaje):
-        print mensaje
-        print mensaje
-        
-class Humano:
-    def _init_(self,edad):
-        self.edad = edad
-        self.alpaca = Olakease(12, 35, 180)
-        self.niño = Humano(12)
-        self.niña = NoBodyToldMeSheWas12(45)
+class Reproductor:
+    "Clase Reproductor Mp3"
+    def __init__(self):
+        pass
+    def reproducirmp3(self):
+        print('reproduciendo mp3')                  
+    def reproducirvideo(self):
+        print('reproduciendo video')                  
 
-    def hablar(self,mensaje):
-        print mensaje
+class Movil(Telefono, Camara, Reproductor):
+    def __del__(self):
+        print('Móvil apagado')
+    def kamara(self):
+        print "penultimo"
+    def iokese(self):
+        print "último"
 
-class NoBodyToldMeSheWas12:
-    def _init_(self, edad):
-        self.edad = edad
-
-    def iokese(self,mensaje):
-        print mensaje
-
-pedro = Humano(26)
-raul = Humano(26)
-
-print 'Soy Pedro y tengo ' + pedro.edad
-print 'Soy Raul y tengo ' + raul.edad
-
-pedro.hablar('Hola')
-raul.hablar('Hola, Pedro')"""
+movil1 = Movil()
+print(dir(movil1))
+movil1.reproducirmp3()
+movil1.telefonear()
+movil1.fotografiar()
+del movil1""")
 
 def buscarNombresClases(pCodigo):
     listaClases = []
@@ -62,29 +63,27 @@ def buscarNombresClases(pCodigo):
     except ValueError: # cuando ya no se encuentre class
         pos_inicial = -1
     for n in range(0, len(listaPosClases)):
-        #Asignamos 30 caracteres más asumiento que el nombre de la clase medirá menos que eso
-        cadenaTMP = codigo[listaPosClases[n] : listaPosClases[n]+30]
+        #Asignamos 100 caracteres más asumiento que el nombre de la clase medirá menos que eso
+        cadenaTMP = codigo[listaPosClases[n] : listaPosClases[n]+100]
         #Obtenemos el nombre de la clase buscando hasta encontrar un punto apartir de de donde se encontró hasta ":"
         #Sumandole 6 posiciones que son las que ocupa "class " para si solo guardar el nombre
-        listaClases.append(codigo[listaPosClases[n]+6:(cadenaTMP.find(":")) + listaPosClases[n]]) 
+        listaClases.append(codigo[listaPosClases[n]+6:cadenaTMP.find(":")+listaPosClases[n]])
     return listaClases
 
-listaClases = buscarNombresClases(codigo)
+#listaClases = buscarNombresClases(codigo)
 
 def buscarMetodos(pClase):
     listaMetodos = []
     listaPosMetodos = []
     pos_inicial = -1
+    #Eliminamos lo anterior a la clase de la cual vamos a obtener los métodos
+    codigoAPartirDeClase1 = codigo[codigo.index(pClase+":"):len(codigo)]
+    #Eliminamos lo posterior a la clase de la cual vamos a obtener los métodos
+    codigoAPartirDeClase2 = codigoAPartirDeClase1[0: codigoAPartirDeClase1.find("class")]
     try:
         while True:
             # cada vez buscamos desde un caracter más adelante de la
             # la última ocurrencia encontrada a partir de la clase que recibimos como parametro
-            # hasta encontrarnos con otra clase buscando hasta el final del codigo
-
-            #Eliminamos lo anterior a la clase de la cual vamos a obtener los métodos
-            codigoAPartirDeClase1 = codigo[codigo.index(pClase):len(codigo)]
-            #Eliminamos lo posterior a la clase de la cual vamos a obtener los métodos
-            codigoAPartirDeClase2 = codigoAPartirDeClase1[0: codigoAPartirDeClase1.find("class")]
             #Se recorre la posición hacia la siguiente de lo último encontrado
             pos_inicial = codigoAPartirDeClase2.index("def", pos_inicial+1)
             listaPosMetodos.append(pos_inicial)
@@ -104,21 +103,17 @@ def buscarAtributos(pClase):
     listaAtributos = []
     listaPosAtributos = []
     pos_inicial = 1
+    #Eliminamos lo anterior a la clase de la cual vamos a obtener los métodos
+    codigoAPartirDeClase1 = codigo[codigo.index(pClase+":"):len(codigo)]
+    #Eliminamos lo posterior a la clase de la cual vamos a obtener los métodos
+    codigoAPartirDeClase2 = codigoAPartirDeClase1[0: codigoAPartirDeClase1.find("class")]
     try:
         while True:
             # cada vez buscamos desde un caracter más adelante de la
             # la última ocurrencia encontrada a partir de la clase que recibimos como parametro
-            # hasta encontrarnos con otra clase buscando hasta el final del codigo
-
-            #Eliminamos lo anterior a la clase de la cual vamos a obtener los métodos
-            codigoAPartirDeClase1 = codigo[codigo.index(pClase):len(codigo)]
-            #Eliminamos lo posterior a la clase de la cual vamos a obtener los métodos
-            codigoAPartirDeClase2 = codigoAPartirDeClase1[0: codigoAPartirDeClase1.find("class")]
             #Se recorre la posición hacia la siguiente de lo último encontrado
             pos_inicial = codigoAPartirDeClase2.index("self.", pos_inicial+1)
             listaPosAtributos.append(pos_inicial)
-            if("class" in codigoAPartirDeClase2[pos_inicial:len(codigo)]):
-                break
     except ValueError: # cuando ya no se encuentre def
         pos_inicial = -1
     for n in range(0, len(listaPosAtributos)):
@@ -133,48 +128,63 @@ def buscarCodigos(pClase):
     listaMetodos = []
     listaPosMetodos = []
     pos_inicial = -1
-    cadenaAUX = ""
     cadenaTMP = ""
+    #Eliminamos lo anterior a la clase de la cual vamos a obtener los métodos
+    codigoAPartirDeClase1 = codigo[codigo.index(pClase+":"):len(codigo)]
+    #Eliminamos lo posterior a la clase de la cual vamos a obtener los métodos
+    codigoAPartirDeClase2 = codigoAPartirDeClase1[0: codigoAPartirDeClase1.find("class")]
     try:
         while True:
             # cada vez buscamos desde un caracter más adelante de la
             # la última ocurrencia encontrada a partir de la clase que recibimos como parametro
-            # hasta encontrarnos con otra clase buscando hasta el final del codigo
-
-            #Eliminamos lo anterior a la clase de la cual vamos a obtener los métodos
-            codigoAPartirDeClase1 = codigo[codigo.index(pClase):len(codigo)]
-            #Eliminamos lo posterior a la clase de la cual vamos a obtener los métodos
-            codigoAPartirDeClase2 = codigoAPartirDeClase1[0: codigoAPartirDeClase1.find("class")]
             #Se recorre la posición hacia la siguiente de lo último encontrado
             pos_inicial = codigoAPartirDeClase2.index("def", pos_inicial+1)
             listaPosMetodos.append(pos_inicial)
-            if("class" in codigoAPartirDeClase2[pos_inicial:len(codigo)]):
-                break
     except ValueError: # cuando ya no se encuentre self
         pos_inicial = -1
-    for n in range(0, len(listaPosMetodos)-1):
-        #Se obtiene una copia de la cadena en la posicion encontrada de n+4 que es donde está el def pero
-        #omitiendo el mismo con esas 4 posiciones de sus caracteres, hasta la longitud del codigo cortado
-        #donde se encuentra un class, para después cortar ese mismo texto desde la posición del def hasta
-        #que se encuentre el siguiente def de tal forma que obtenemos todo el contenido del codigo sin
-        #importar su longitud, para obtener el contenido de todos los metodos menores a la longitud de la
-        #cantidad de métodos existentes en una clase, esto quiere decir que el último no se obtiene debido
-        #a que buscamos un "def" y el último elemento nunca va encontrar uno, por lo que lo añadiremos de
-        #una forma diferente
-        cadenaAUX = codigoAPartirDeClase2[listaPosMetodos[n]+4 : len(codigoAPartirDeClase2)]
-        cadenaTMP = cadenaAUX[0: cadenaAUX[n].find("def")] 
+    for n in range(0, len(listaPosMetodos)):
+        #Se obtiene una copia de la cadena en la posicion encontrada de n+4 que es donde está el "def "
+        #hasta la longitud del codigo cortado donde se encuentra un class, de tal forma que obtenemos
+        #todo el contenido del codigo sin importar su longitud
+        cadenaTMP = codigoAPartirDeClase2[listaPosMetodos[n]+4 : len(codigoAPartirDeClase2)]
         #Se obteiene el codigo de la función desechando de la cadena temporal todo aquello que no sea eso
-        listaMetodos.append(codigoAPartirDeClase2[listaPosMetodos[n]+5+cadenaTMP.find(":"):(cadenaTMP.find("def")) + listaPosMetodos[n]])
-    #Se añade el contenido del último método buscando en el texto donde aún no se ha quitado el contenido
-    #que tiene dentro al "class" para delimitar donde acaba ese método
-    listaMetodos.append(codigoAPartirDeClase1[listaPosMetodos[len(listaPosMetodos)-1]+cadenaTMP.find(":")-5:codigoAPartirDeClase1.find("class")-1])
+        #recorriendo desde el inicio de la función hasta encontral el siguiente "def"
+        listaMetodos.append(cadenaTMP[cadenaTMP.find(":")+1:cadenaTMP.find("def")])
+    return listaMetodos
+
+def buscarCodigoMetodoUltimaClase(pClase):
+    listaMetodos = []
+    listaPosMetodos = []
+    pos_inicial = -1
+    cadenaTMP = ""
+    #Eliminamos lo anterior a la clase de la cual vamos a obtener los métodos
+    codigoAPartirDeClase1 = codigo[codigo.index(pClase+":"):len(codigo)]
+    #Eliminamos lo posterior a la clase de la cual vamos a obtener los métodos
+    codigoAPartirDeClase2 = codigoAPartirDeClase1[0: codigoAPartirDeClase1.find("\n\n")]
+    try:
+        while True:
+            # cada vez buscamos desde un caracter más adelante de la
+            # la última ocurrencia encontrada a partir de la clase que recibimos como parametro
+            #Se recorre la posición hacia la siguiente de lo último encontrado
+            pos_inicial = codigoAPartirDeClase2.index("def", pos_inicial+1)
+            listaPosMetodos.append(pos_inicial)
+    except ValueError: # cuando ya no se encuentre self
+        pos_inicial = -1
+    for n in range(0, len(listaPosMetodos)):
+        #Se obtiene una copia de la cadena en la posicion encontrada de n+4 que es donde está el "def "
+        #hasta la longitud del codigo cortado donde se encuentra un class, de tal forma que obtenemos
+        #todo el contenido del codigo sin importar su longitud
+        cadenaTMP = codigoAPartirDeClase2[listaPosMetodos[n]+4 : len(codigoAPartirDeClase2)]
+        #Se obteiene el codigo de la función desechando de la cadena temporal todo aquello que no sea eso
+        #recorriendo desde el inicio de la función hasta encontral el siguiente "def"
+        listaMetodos.append(cadenaTMP[cadenaTMP.find(":")+1:cadenaTMP.find("def")])
     return listaMetodos
 
 def buscarRelaciones(pClase):
     clasesRelacionadas = []
     listaClases = buscarNombresClases(codigo)
     #Eliminamos lo anterior a la clase de la cual vamos a obtener los métodos
-    codigoAPartirDeClase1 = codigo[codigo.index(pClase):len(codigo)]
+    codigoAPartirDeClase1 = codigo[codigo.index(pClase+":"):len(codigo)]
     #Eliminamos lo posterior a la clase de la cual vamos a obtener los métodos
     codigoAPartirDeClase2 = codigoAPartirDeClase1[0: codigoAPartirDeClase1.find("class")]
     #Buscamos todas las instanciaciones de objetos dentro de la clase
@@ -201,7 +211,10 @@ def obtenerJson():
         #Se asignan los métodos de la clase al atributo del diccionario del mismo nombre
         plantillaDiccionario['metodos'] = buscarMetodos(listaClases[n])
         #Se asignan los contenidos de los metodos de la clase al atributo del diccionario del mismo nombre
-        plantillaDiccionario['contenidos'] = buscarCodigos(listaClases[n])
+        if(n == len(listaDiccionarios)-1):
+            plantillaDiccionario['contenidos'] = buscarCodigoMetodoUltimaClase(listaClases[n])
+        else:
+            plantillaDiccionario['contenidos'] = buscarCodigos(listaClases[n])
         #Se asignan las relaciones de las clases al atributo del diccionario del mismo nombre
         plantillaDiccionario['relaciones'] = buscarRelaciones(listaClases[n])
         #Se guarda el diccionario actual en la lista para no perderlo
@@ -218,7 +231,8 @@ listaMetodosAlpaca = buscarMetodos("Olakease")
 listaMetodosNobody = buscarMetodos("NoBodyToldMeSheWas12")
 listaAtributosAlpaca = buscarAtributos("Olakease")
 listaAtributosHumano = buscarAtributos("Humano")"""
-
+#listaCodigos1 = buscarCodigos("Movil(Telefono, Camara, Reproductor)")
+#print listaCodigos1
 """print listaClases
 print listaMetodosHumano
 print listaMetodosAlpaca
